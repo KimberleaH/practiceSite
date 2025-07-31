cf40a24de32b7e65e0d01525be5a29d32c381be7
Kimberlea Heili <62309455+KimberleaH@user.noreply.github.com>
2025-07-30 21:24:51 -0500

    add javascript to service slider

---
+++ script.js
@@ -0,0 +1,14 @@
+const serviceSlider = document.getElementById("serviceSlider");
+const testimonialSlider = document.getElementById("testimonialSlider");
+
+
+function slideLeft(element) {
+    element.scrollBy(-400, 0);
+};
+
+function slideRight(element) {
+    element.scrollBy(400, 0);
+};
+
+
+
